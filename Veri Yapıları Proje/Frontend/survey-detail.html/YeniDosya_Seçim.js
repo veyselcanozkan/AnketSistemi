5c446d7e983f9808d4c7e4cb35de2ef44631b748
İbrahim <150539495+ibrahimCali27@users.noreply.github.com>
2025-05-04 18:09:09 +0300

    Add files via upload

---
+++ Veri Yapıları Proje/Frontend/survey-detail.html/YeniDosya_Seçim.js
@@ -0,0 +1,26 @@
+document.getElementById("loginForm").addEventListener("submit",async function a(event){
+    const e_Posta=document.getElementById("email").value;
+    const sifre=document.getElementById("password").value;
+    try{
+        let response=await fetch(""//Node.Js buraya gelecektir(?)
+            ,{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify({e_Posta,sifre})
+        });
+        let sonuc=await response.json();
+        if (response.ok) {
+            console.log("Token:", sonuc.token);
+            localStorage.setItem("token", sonuc.token);
+            window.location.href="";//Hesap sitesi ya da anasayfaya atmalıdır.
+        } else {
+            alert("Giriş başarısız: " + result.message);
+        }
+    } catch (error) {
+        console.error("Hata:", error);
+        backend($response);
+    }
+});
+function backend(){
+
+}
